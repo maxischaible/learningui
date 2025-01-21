b8030eb5ed2cfe61dba6634be95a227165ad5a99
FlorianScherlFASTAIMovies <florian.scherl@fast-ai-movies.com>
2025-01-21 11:00:24 +0100

    Making it multiPage ready

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
     }
   }, []);
   useEffect(() => {
-    ReactGA.initialize('G-96T9B1QEVK');
+    ReactGA.initialize('G-QD64EYCST5');
     // To Report Page View 
     ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
   }, [])
--- src/context/ViewContext.tsx
+++ src/context/ViewContext.tsx
@@ -42,7 +42,7 @@ interface ViewContextType {
 const ViewContext = createContext<ViewContextType | undefined>(undefined);
 
 export const ViewProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [isExpanded, setIsExpanded] = useState(true);
+  const [isExpanded, setIsExpanded] = useState(false);
   const [videos, setVideos] = useState<Video[]>([]);
   const [summaryData, setSummaryData] = useState<SummaryData | null>(null);
   const [linkedElements, setLinkedElements] = useState<LinkedElement[]>([]);
